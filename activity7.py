ace5324e32d0f2cf82f3afe0a787f8c6d1089c30
MorconJm <jannamariemorcon13@gmail.com>
2026-09-06 20:00:25 +0800

    Add files via upload
     Assignment Operator.

---
+++ activity7.py
@@ -0,0 +1,13 @@
+# assignment operator
+x = 0
+
+n1 = eval(input("calculate-->"))
+total = x + n1 
+
+n2 = eval(input("calculate--->")) 
+total += n2 
+
+n3 = eval(input("calculate--->")) 
+total += n3
+
+print("--->", total)
