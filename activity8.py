b1301107d8ce1db0b735c2c7cd4fbc0056bf6ce3
MorconJm <jannamariemorcon13@gmail.com>
2026-09-06 20:03:44 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,12 @@
+food = ""
+
+fb = input("what food do you like?")
+food += fb + ","
+
+fb = input("flavour?") 
+food += fb + ","
+
+fb = input("others?")
+food += fb + "."
+
+print("My preference are", food)
