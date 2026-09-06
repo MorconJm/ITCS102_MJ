7948fbf34b3b8c9080271e065d1d597726c819ea
MorconJm <jannamariemorcon13@gmail.com>
2026-09-06 19:31:25 +0800

    Add files via upload
    4 Types of Variables.

---
+++ activity3.py
@@ -0,0 +1,25 @@
+#Integer Variable
+unit = 26
+age = 18
+student_number = 11
+
+
+#Float Variable
+price = 10.99
+pity= 90.99
+guranteed = 8.99
+
+#String Variable
+name = "mj"
+level = "college"
+username = "bananaman"
+#Boolen Variable
+
+isMarried = False
+isEnrolled = True
+isFemale = True
+
+print("Hello my name is",name)
+print("Im in my first year of",level)
+print("Im enrolled",isEnrolled)
+print("Im",age)
