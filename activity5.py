ef48bbe3bc879b3ae8cd39a5e0ff1dc5274653f3
MorconJm <jannamariemorcon13@gmail.com>
2026-09-06 19:47:25 +0800

    Add files via upload
    Arithmetic operators.

---
+++ activity5.py
@@ -0,0 +1,52 @@
+#operators
+#arithmetic operators
+
+# +- addition
+# -- subtraction
+# * - multiplication
+# /- division
+# // - floor division
+# ** - exponential
+# % - modulos
+
+#Addition
+
+a = 20
+b = 5
+
+sum = a + b
+
+print("The sum of a and b is",sum)
+
+#Difference
+
+c = 20
+d = 5
+
+diff = c - d
+
+print("The difference of a and b is",diff)
+
+
+#Product
+e = 20
+f = 5
+
+product = e * f
+
+print("The product of a and b is",product)
+
+#Quotient
+
+g = 20
+h = 5
+
+quo = g / h
+
+print("The quotient of a and b is",quo)
+
+
+
+
+
+
